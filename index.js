522609d303e94bbbdd7fa4942d25a56c894221bc
drftgyhu8j <gs1retewerw@hotmail.com>
2024-05-08 23:47:25 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function averageArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+function combine(n, k) {
+  const result = [];
+  backtrack(1, []);
+  return result;
+  function backtrack(start, current) {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      current.push(i);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
 }
