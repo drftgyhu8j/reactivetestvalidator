3538c789cb02751494b4e29e37f153e1e6fe3ac3
drftgyhu8j <gs1retewerw@hotmail.com>
2024-05-09 18:14:01 +0700

    aptos

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function combine(n, k) {
-  const result = [];
-  backtrack(1, []);
-  return result;
-  function backtrack(start, current) {
-    if (current.length === k) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = start; i <= n; i++) {
-      current.push(i);
-      backtrack(i + 1, current);
-      current.pop();
+function numDistinct(s, t) {
+  const m = s.length;
+  const n = t.length;
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s[i - 1] === t[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
     }
   }
+  return dp[m][n];
 }
