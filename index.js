6854f0b77c63e206be06c6181c99d19abb129e2f
drftgyhu8j <gs1retewerw@hotmail.com>
2024-05-12 00:54:18 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
+function productExceptSelf(nums) {
+  const n = nums.length;
+  const result = new Array(n).fill(1);
+  let product = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  product = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  return result;
+}
